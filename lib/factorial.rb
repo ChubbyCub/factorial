c3a2f03b9c5e822833a2e9b7b69ff8a85d0b29ac
Elise Pham <elise.pham88@gmail.com>
2019-03-24 10:45:03 -0700

    completed factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,20 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def factorial(number)
-  raise NotImplementedError
+  if number.nil?
+    raise ArgumentError.new("Input cannot be nil")
+  end
+
+  factorial = 1
+
+  if number == 0
+    return factorial
+  end
+
+  for i in 1..number
+    factorial *= i
+  end
+
+  return factorial
 end
